97b81f50829f727104a4f6af23a81cadfbfba7da
Sreenath Maniyeeri <mvsreenath@gmail.com>
2017-10-24 22:37:28 +0530

    n8

--- app/src/main/java/mango/salt/memtraining/MainActivity.java
+++ app/src/main/java/mango/salt/memtraining/MainActivity.java
@@ -1,5 +1,6 @@
 package mango.salt.memtraining;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void ShortTermVisualMemory(View view) {
+        Intent intent = new Intent(this,ShortTermVisualMemory.class);
+        startActivity(intent);
     }
 }
